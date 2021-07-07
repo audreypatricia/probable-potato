348cad4ee6c4f7223fbf61924786c3f11c2553ba
Audrey Patricia <audreypatricia@Audreys-MacBook-Pro.local>
2021-07-07 14:28:33 +1000

    day 6 - functions

---
+++ 06-functions/main.py
@@ -0,0 +1,20 @@
+# Functions
+def my_function():
+  print("Hello")
+  print("Bye")
+
+my_function() # use this to call the function
+
+# in python indentation is 4 spaces
+
+#While loop
+# while something_is_true:
+    #Do something repeatedly
+
+number_of_people = 3
+while number_of_people > 0:
+    print("hello")
+    number_of_people -= 1
+
+
+# exercises were done on Reeborg's world!
