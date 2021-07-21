ac9ee7239ddaddf9085ffb3e4f2ca42f3f2da041
audreypatricia <audreypatricia1101@gmail.com>
2021-07-21 12:01:36 +1000

    day 20 - snake game part 1

---
+++ 20-snake_game_part1/snake.py
@@ -0,0 +1,52 @@
+from turtle import Turtle
+
+X_POS = [0, -20, -40]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for i in range(0, 3):
+            snake_seg = Turtle("square")
+            snake_seg.color("white")
+            snake_seg.penup()
+            snake_seg.setpos(X_POS[i], 0)
+            self.segments.append(snake_seg)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.segments[0].setheading(LEFT)
+
+
+
+
+
+
